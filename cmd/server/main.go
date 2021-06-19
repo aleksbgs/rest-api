a23d46533740aa304bead3e0e34984b40ecf2bde
Aleksandar Markovic <aleksandar.markovic@firstbeatmedia.com>
2021-06-19 22:02:00 +0200

    Added Health check point

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	transportHTTP "github.com/aleksbgs/rest-api/internal/transport/http"
+	"net/http"
+)
 
-func main()  {
+// App- the struct which contains things like pointers
+// to database connection
+type App struct{}
+
+// Run- sets up application
+func (app *App) Run() error {
+	fmt.Println("Settings Up Our APP")
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("Failed to set up server")
+		return err
+	}
+	return nil
+}
+
+func main() {
 	fmt.Println("Go rest APi Course")
+
+	app := App{}
+
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up our REST API")
+		fmt.Println(err)
+	}
 }
